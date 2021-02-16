af78a35d203ab497627318045d80891e8e05c215
Arthur Valentim <valentin.arthur1000@gmail.com>
2021-02-16 10:16:07 +0100

    Modification menu déroulant bas de page

--- script.js
+++ script.js
@@ -7,5 +7,40 @@ $(document).ready(function(){
         
 
     });
+    $('.button2').click(function () {
+        $('.ac, .bc').toggle(function () {
+            $('.bc').addClass("display:block");
+        })
+
+
+    });
+    $('.button3').click(function () {
+        $('.ac, .cd').toggle(function () {
+            $('.cd').addClass("display:block");
+        })
+
+
+    });
+    $('.button4').click(function () {
+        $('.ac, .de').toggle(function () {
+            $('.de').addClass("display:block");
+        })
+
+
+    });
+    $('.button5').click(function () {
+        $('.ac, .ef').toggle(function () {
+            $('.ef').addClass("display:block");
+        })
+
+
+    });
+    $('.button6').click(function () {
+        $('.ac, .fg').toggle(function () {
+            $('.fg').addClass("display:block");
+        })
+
+
+    });
 
 });
