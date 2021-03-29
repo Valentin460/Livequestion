c90e6bd510849b88b17f00d2b7f346c7c8a32610
Arthur Valentim <valentin.arthur1000@gmail.com>
2021-03-29 14:28:41 +0200

    Ajout PHP page vue d'une question

--- vuequestion.php
+++ vuequestion.php
@@ -11,16 +11,36 @@
         <script src="script.js"></script>
     </head>
     <body>
+    <?php
+        require_once("db/fonctions.php");
+        $co = connexionBdd();
+    ?>
         <h1>Bienvenue Utilisateur</h1>
         <div class="general">
             <div class="left">
-                <h2>Titre de la question :</h2>
-                <h2>Date de création :</h2>
+                <h2>Titre de la question :
+                <?php
+                    $reponse = $co->query("SELECT titre FROM questions");
+                ?>
+                </h2>
+                <h2>Date de création :
+                <?php
+                    $reponse = $co->query("SELECT date_crea FROM questions");
+                ?>
+                </h2>
                 <h2>Nombre de réponses obtenues :<h2>
             </div>
             <div class="right">
-                <h2>Nom d'utilisateur :</h2>
-                <h2>Contenu de la réponse :</h2>
+                <h2>Nom d'utilisateur :
+                <?php
+                    $reponse = $co->query("SELECT pseudo FROM utilisateurs");
+                ?>
+                </h2>
+                <h2>Contenu de la réponse :
+                <?php
+                    $reponse = $co->query("SELECT rep_quest FROM reponses");
+                ?>
+                </h2>
             </div>
         </div>
     </body>
