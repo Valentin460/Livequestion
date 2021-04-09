b2920beb13c27c4a3472c76b9e9d74e357c9f51f
Arthur Valentim <valentin.arthur1000@gmail.com>
2021-04-09 15:04:14 +0200

    Ajout informations dans le sélecteur

--- ajoutquestion.php
+++ ajoutquestion.php
@@ -15,9 +15,9 @@
     <?php
         require('db/fonctions.php');
         if(isset($_POST['submit'])){
-            if(isset($_POST['titrequestion'], $_POST['cat'])){
+            if(isset($_POST['titrequestion'], $_POST['categorie'])){
                 $titre = $_POST['titrequestion'];
-                $cat_id = $_POST['cat'];
+                $cat_id = $_POST['categorie'];
 
                 $co = connexionBdd();
 
@@ -31,6 +31,19 @@
         }
         else{
     ?>
+
+        <?php
+            require_once("db/fonctions.php");
+
+            if (isset($_POST['categorie']))
+            {
+                $categorie = $_POST['categorie'];
+            }
+            else
+            {
+                $categorie = "sport";
+            }
+        ?>
         <div id="color">
             <div class="container">
                 <div id="inscription">
@@ -43,10 +56,19 @@
                         <div class="form-group">
                             <label for="categorie" id="cat">Catégorie</label>
                             <select class="form-control" name="catetogie" id="selectCat">
-                                <option name="cat">Cinéma</option>
-                                <option name="cat">Maths</option>
-                                <option name="cat">Français</option>
-                                <option name="cat">Technologie</option>
+                                <option value="sport" <?php if ($categorie=='sport') { echo 'selected';} ?>>Sport</option>
+                                <option value="musique" <?php if ($categorie=='musique') { echo 'selected';} ?>>Musique</option>
+                                <option value="cinema" <?php if ($categorie=='cinema') { echo 'selected';} ?>>Cinéma</option>
+                                <option value="jeux" <?php if ($categorie=='jeux') { echo 'selected';} ?>>Jeux</option>
+                                <option value="histoire" <?php if ($categorie=='histoire') { echo 'selected';} ?>>histoire</option>
+                                <option value="science" <?php if ($categorie=='science') { echo 'selected';} ?>>science</option>
+                                <option value="politique" <?php if ($categorie=='politique') { echo 'selected';} ?>>Politique</option>
+                                <option value="television" <?php if ($categorie=='television') { echo 'selected';} ?>>Télévision</option>
+                                <option value="art" <?php if ($categorie=='art') { echo 'selected';} ?>>Art</option>
+                                <option value="gastronomie" <?php if ($categorie=='gastronomie') { echo 'selected';} ?>>Gastronomie</option>
+                                <option value="voyage" <?php if ($categorie=='voyage') { echo 'selected';} ?>>Voyage</option>
+                                <option value="geographie" <?php if ($categorie=='geographie') { echo 'selected';} ?>>Géographie</option>
+                                <option value="economie" <?php if ($categorie=='economie') { echo 'selected';} ?>>Economie</option>
                             </select>
                         <button type="submit" class="btn btn-primary" id="but">Ajouter la question</button>
                     </form>
