6e297fdaceb78f63e221bae229eb94a2f1052c3a
Aleksandar <a.milenkovic321@gmail.com>
2021-04-12 15:21:29 +0200

    Amélioration de la page de connexion.

--- connexion.php
+++ connexion.php
@@ -23,10 +23,10 @@
 				$pseudo = $_POST['pseudo'];
 				$mot_de_passe = hash('sha256', $_POST['password']);
 
-				$query = $co->prepare('SELECT * FROM utilisateurs WHERE pseudo=:login and password=:pass');
+				$query = $co->prepare('SELECT * FROM utilisateurs WHERE pseudo=:login and mot_de_passe=:pass');
 
-				$query->bindParam(':pseudo', $pseudo);
-				$query->bindParam(':mot_de_passe', $mot_de_passe);
+				$query->bindParam(':login', $pseudo);
+				$query->bindParam(':pass', $mot_de_passe);
 
 				$query->execute();
 
@@ -35,11 +35,11 @@
 				$rows = $query->rowCount();
 
 				if ($rows==1){
-					$_SESSION['username'] = $username;
+					$_SESSION['pseudo'] = $pseudo;
 
 					header("Location: vuemembre.php");
 				}else{
-					$message = "Le nom d'utilisateur ou le mot de passe est incorrect";
+					$message = " Le nom d'utilisateur ou le mot de passe est incorrect";
 				}
 		}
 		?>
@@ -47,7 +47,7 @@
 			<div class="container">
 				<div id="inscription">
 					<h2><span class="span">Connexion<span></h2>
-					<form id="formulaire" method="post">
+					<form id="formulaire" method="post" name="login">
 						<div class="form-group">
 							<input type="text" name="pseudo" class="form-control" placeholder="Pseudo" id="name">
 						</div>
@@ -55,11 +55,11 @@
 							<input type="password" name="password" id="mdp" class="form-control" placeholder="Mot de passe">
 						</div>
 						<div>
-							<button type="submit" class="btn btn-primary" id="but">Se connecter</button>
+							<button type="submit" name="submit" class="btn btn-primary" id="but">Se connecter</button>
 						</div>
 						<?php
 							if (!empty($message)){ ?>
-							<p><?php echo $message; ?></p>
+							<p class="warning"><i class="fas fa-exclamation-triangle"></i><?php echo $message; ?></p>
 							<?php } ?>
 					</form>
 					<div>
