861f1e834d6a3dcbff736364ec43d824aa8eae74
Arthur Valentim <valentin.arthur1000@gmail.com>
2021-04-15 18:43:29 +0200

    Rajout du fichier head.php sur les différentes pages

--- vuemembre/vuemembre.php
+++ vuemembre/vuemembre.php
@@ -1,17 +1,5 @@
-﻿<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8" />
-	<title>Vue d'un membre</title>
-	<link rel="stylesheet" type="text/css" href="vuemembre.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-	<script src="https://kit.fontawesome.com/d8cf10d130.js" crossorigin="anonymous"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-</head>
-<body>
-<?php
+﻿<?php
+	include("../includes/head.php");
 	include("../includes/connexion-head.php");
 
 	// Récupération des données de la session
