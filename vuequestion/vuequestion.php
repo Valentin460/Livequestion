51644e0eaa28edab05f6f9785d606ab3588020ce
Aleksandar <a.milenkovic321@gmail.com>
2021-04-20 13:35:09 +0200

    Modification de la page de vue d'une question.

--- vuequestion/vuequestion.php
+++ vuequestion/vuequestion.php
@@ -3,34 +3,44 @@
         require_once("../db/fonctions.php");
         $co = connexionBdd();
     ?>
-        <h1>Bienvenue Utilisateur</h1>
-        <div class="general">
-            <div class="left">
-                <h2>Titre de la question :
-                <?php
-                    $reponse = $co->query("SELECT titre FROM questions");
-                ?>
-                </h2>
-                <h2>Date de création :
-                <?php
-                    $reponse = $co->query("SELECT date_crea FROM questions");
-                ?>
-                </h2>
-                <h2>Nombre de réponses obtenues :<h2>
+        <section>
+            <div id="quest" class="container">
+            	<li>
+                    <ul>Avatar</ul>
+                    <ul>Nom</ul>
+                    <ul>Nb Réponses</ul>
+                    <ul>Catégories (beaucoup de Select à faire ici)</ul>
+                </li>
+                <p>Question</p>
             </div>
-            <div class="right">
-                <h2>Nom d'utilisateur :
-                <?php
-                    $reponse = $co->query("SELECT pseudo FROM utilisateurs");
-                ?>
-                </h2>
-                <h2>Contenu de la réponse :
-                <?php
-                    $reponse = $co->query("SELECT rep_quest FROM reponses");
-                ?>
-                </h2>
+            <div id="rep" class="container">
+                <div id="reponse">
+                    <h4><span>2</span> Réponse</h4>
+                    <div>
+                        <h6>Avatar + Pseudo</h6>
+                        <p>Réponse à la question (Select ici aussi)</p>
+                    </div>
+                </div>
+                <div id="repondre">
+                    <h4>Répondre à la question</h4>
+                    <form method="post">
+                        <div class="form-group">
+                            <label>Votre pseudo</label>
+                            <input type="text" name="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Lien web vers votre avatar</label>
+                            <input type="text" name="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Votre réponse</label>
+                            <textarea class="form-control"></textarea>
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary" id="but">Valider</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </section>
     <?php
         include("../includes/connexion-footer.php");
     ?>
