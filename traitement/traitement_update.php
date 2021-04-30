bc898aa75bf495faf5f470cb462fe94e9c30bd81
Aleksandar <a.milenkovic321@gmail.com>
2021-04-30 19:31:04 +0200

    Modification de la vérification de traitement_update.

--- traitement/traitement_update.php
+++ traitement/traitement_update.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
     require '../db/fonctions.php';
 
@@ -56,6 +56,38 @@
         $avatar = $item['avatar_utilisateur'];
         $id = $item['id_utilisateur'];
     }
+
+    if(isset($_FILES["image"]) && $_FILES["image"]["error"] == 0){
+				$allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+				$filename = $_FILES["image"]["name"];
+				$filetype = $_FILES["image"]["type"];
+				$filesize = $_FILES["image"]["size"];
+
+				// Vérifie l'extension du fichier
+				$ext = pathinfo($filename, PATHINFO_EXTENSION);
+				if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
+
+				// Vérifie la taille du fichier - 5Mo maximum
+				$maxsize = 5 * 1024 * 1024;
+				if($filesize > $maxsize) die("Error: La taille du fichier est supérieure à la limite autorisée.");
+
+                // Vérifie le type MIME du fichier
+				if(in_array($filetype, $allowed)){
+					// Vérifie si le fichier existe avant de le télécharger.
+					if(file_exists("../images/" . $_FILES["image"]["name"])){
+						echo $_FILES["image"]["name"] . " existe déjà.";
+					} else{
+						move_uploaded_file($_FILES["image"]["tmp_name"], "../images/" . $_FILES["image"]["name"]);
+						echo "Votre fichier a été téléchargé avec succès.";
+					} 
+				} else{
+					echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer."; 
+				}
+				}
+
+                $avatar = strtr($avatar,
+				'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜİàáâãäåçèéêëìíîïğòóôõöùúûüıÿ',
+				'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy'); 
 
     function checkInput($data) 
     {
